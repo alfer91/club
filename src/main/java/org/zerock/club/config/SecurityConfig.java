a83ac4435cecf08ac6e49b1b92933e58351f90ff
alfer91 <dasungwkd@naver.com>
2021-09-30 16:16:24 +0900

    인가(Authorizastion)가 필요한 리소스 설정

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,5 +26,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("$2a$10$Yyy76hlkUG5SErw9PacolebmdH22ezxheFiJDEBbqkeYDUnOvy9NO")
                 .roles("USER");
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        http.formLogin(); //인가/인증에 문제시 로그인 화면
     }
 }
