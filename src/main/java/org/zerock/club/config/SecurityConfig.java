0bb523db5e3e44ab755fafa45d002d6ac1f0c661
alfer91 <dasungwkd@naver.com>
2021-09-30 16:26:55 +0900

    CSRF 설정

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -36,5 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/sample/member").hasRole("USER");
 
         http.formLogin(); //인가/인증에 문제시 로그인 화면
+        http.csrf().disable();
+        http.logout();
     }
 }
